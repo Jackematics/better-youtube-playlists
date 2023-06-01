0db9c0b807374e3fea29c763aaac3026629c3bec
Jack Rimmer <jackrimmer2996@hotmail.co.uk>
2023-06-01 13:59:05 +0100

    Scale on rem instead of screen %

--- src/app/components/playlist-list/PlaylistList.tsx
+++ src/app/components/playlist-list/PlaylistList.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const PlaylistList = () => {
     return (
         <>
-            <div className="w-1/6 h-screen bg-container-dark-blue m-5 border-4 relative">
+            <div className="w-72 h-screen bg-container-dark-blue m-5 border-4 relative">
                 <h2 className="text-white text-5xl font-bold text-shadow-black flex justify-center mt-5 mb-5">Playlists</h2>
                 <div className="pl-6 flex flex-col">
                     <button className="text-white text-3xl text-shadow-black text-left mb-2 rounded-2xl w-max pl-4 pr-4 pb-1 pt-1 hover:bg-hover-highlight-blue hover:text-black hover:text-shadow-white focus:bg-select-highlight-blue focus:text-black focus:text-shadow-white">Synth Haven</button>
